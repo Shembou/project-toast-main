20c7f1553ffdce2db195b76877b0b0983ad5ae35
Shembou <92935278+Shembou@users.noreply.github.com>
2023-09-19 15:19:48 +0200

    Exercise 5.1

--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -3,6 +3,20 @@ import React from 'react';
 export const ToastContext = React.createContext();
 
 function ToastProvider({children}) {
+
+  React.useEffect(() => {
+    function handleKeyDown(event) {
+      if (event.code === 'Escape') {
+        setToasts([]);
+      }
+    }
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
   
   const [selectedVariant, setSelectedVariant] = React.useState("notice");
 
